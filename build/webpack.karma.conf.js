7f36920b93ad2c428ff95d09b2e724dc7bd0203b
onsinsin <onsinsin@163.com>
2018-08-04 14:11:50 +0800

    init&js unit test

---
+++ build/webpack.karma.conf.js
@@ -0,0 +1,36 @@
+/**
+ * Created by onsinsin on 2018/7/26.
+ */
+const webpack = require('webpack')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const path = require('path')
+function resolve(dir) {
+  return path.join(__dirname, '..', dir)
+}
+module.exports = {
+  resolve: {
+    extensions: ['.js'],
+    alias: {}
+  },
+  externals: [],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: [resolve('src')]
+      }
+    ]
+  },
+  plugins: [
+    new UglifyJsPlugin({
+      uglifyOptions: {
+        compress: {
+          warnings: false
+        }
+      },
+      sourceMap: true,
+      parallel: true
+    })
+  ]
+}
